e2e8fd18780c5f539e68dcc3909ee2ee12c24ace
Luis Gustavo <luis.furtado@mob2con.com.br>
2019-04-24 15:59:35 -0300

    stage

--- src/rsf-default-fields/radioButton.tsx
+++ src/rsf-default-fields/radioButton.tsx
@@ -2,13 +2,13 @@ import * as React from 'react'
 import { Field, PureFieldProps, fProps, notFProps } from '../react-satisfying-forms/field';
 
 export const RadioButtonField = (props: PureFieldProps & React.InputHTMLAttributes<HTMLInputElement>) => 
-        <Field {...fProps(props)} fUseDebounce={false} fUseFormStateValue>
+        <Field {...fProps(props)} fUseFormStateValue fUseDebounce={false}>
             {(fieldData) => {
                 return <input 
                     {...notFProps(fieldData, props)}
                     type={'radio'} 
                     checked={fieldData.value === props.value}
                     />
-                }
+                } 
             }
         </Field>
--- src/satisfyingFormExample.tsx
+++ src/satisfyingFormExample.tsx
@@ -5,6 +5,7 @@ import { SelectField } from './rsf-default-fields/selectField';
 import { TextAreaField } from './rsf-default-fields/textArea';
 import { CheckboxField } from './rsf-default-fields/checkboxField';
 import { RadioButtonField } from './rsf-default-fields/radioButton';
+import { FieldGroup } from './react-satisfying-forms/fieldGroup';
 
 interface DTOPessoa {
     nome: string
@@ -38,23 +39,27 @@ export function SatisfyingFormExample() {
                         <option>Opt 3</option>
                     </SelectField>
                 </div>
+                
                 {state.fieldValues.estrangeiro && 
                     <div>
                         <div>Descriço do pais</div>
                         <TextAreaField fName='text'/>
                     </div>
                 }
-                <div>
-                    <div>Checkbox</div>
-                    <CheckboxField fName='estrangeiro'/> Checkbox 1
-                </div>
+                
+                <FieldGroup name={'external_data'}>
+                    <div>
+                        <div>Checkbox</div>
+                        <CheckboxField fName='forign'/> Checkbox 1
+                    </div>
 
-                <div>
-                    <div>RadioButtons</div>
-                    <RadioButtonField fName='coutry' value='brazil' fInspect/> Brazil
-                    <RadioButtonField fName='coutry' value='usa' fInspect/> USA
-                    <RadioButtonField fName='coutry' value='angola' fInspect/> Angola
-                </div>
+                    <div>
+                        <div>RadioButtons</div>
+                        <RadioButtonField fName='coutry' value='brazil'/> Brazil
+                        <RadioButtonField fName='coutry' value='usa' /> USA
+                        <RadioButtonField fName='coutry' value='angola' /> Angola
+                    </div>
+                </FieldGroup>
             </React.Fragment>
         }
     </Form>
