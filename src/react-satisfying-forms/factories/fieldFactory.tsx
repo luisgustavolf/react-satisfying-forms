fcc553d8f8f91f3bb2da58dcd0fecf9b0bd32482
Luis Gustavo <luisgustavolf@gmail.com>
2019-04-28 18:00:46 -0300

    stage

--- src/react-satisfying-forms/factories/fieldFactory.tsx
+++
@@ -1,9 +0,0 @@
-import { ContextedFieldProps, ContextedField } from "../contextedField";
-import * as React from "react";
-
-export function DefaultFieldFactory<TFieldProps>(InnerField: (props: TFieldProps) => React.ReactComponentElement<any, any>) {
-    return (props: TFieldProps & ContextedFieldProps) => 
-        <ContextedField {...props}>
-            {(fieldData) =>  <InnerField {...props} {...fieldData}></InnerField>}
-        </ContextedField>
-}
