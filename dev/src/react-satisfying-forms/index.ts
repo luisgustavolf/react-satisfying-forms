28cb4b35d10410af6e5ae753d67ea40056dc854f
Luis Gustavo <luisgustavolf@gmail.com>
2019-04-30 20:47:45 -0300

    inclusao do npm packing

---
+++ dev/src/react-satisfying-forms/index.ts
@@ -0,0 +1,3 @@
+export { Form } from './form';
+export { Field } from './field';
+export { FieldGroup } from './fieldGroup';
---
+++ npm-packing/updater.js
@@ -0,0 +1,34 @@
+const fse = require('fs-extra');
+const exec = require('child_process').exec;
+const c = require('chalk').default;
+
+console.log(c.red('Limpando compilacoes antigas...'))
+fse.emptyDir('./dist')
+    .then(() => {
+        console.log(c.red('Limpando sources antigos...'))
+        return fse.emptyDir('./src')
+    })
+    .then(() => {
+        console.log(c.green('Copiando novos sources...'))
+        return fse.copy('../dev/src/react-satisfying-forms', './src')
+    })
+    .then(() => {
+        console.log(c.green('Compilando...'))
+        return compilaSources()
+    }).then(() => {
+        console.log(c.green('Tudo pronto!'))
+    }).catch((err) => {
+        console.log(c.red('Prrr'))
+        console.log(c.red(err))
+    })
+
+
+function compilaSources() {
+    return new Promise((resolve, reject) => {
+        exec('tsc', (error, stdout, stderr) => {
+            if (error) 
+                reject(stderr)
+            resolve(stdout)
+        })
+    })
+}
