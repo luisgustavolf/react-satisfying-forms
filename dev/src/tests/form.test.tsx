2f622430aa96066c9cc40736710546b038727dbe
Luis Gustavo <luis.furtado@mob2con.com.br>
2019-05-02 18:43:16 -0300

    test scenarios

--- dev/src/tests/form.test.tsx
+++ dev/src/tests/form.test.tsx
@@ -9,7 +9,7 @@ beforeAll(() => {
 
 it ('Sets values from model correcly', () => {
     const form = mount(
-        <Form>
+        <Form >
             {(submit, state) => 
                 <Field fName='first.second' fUseDebounce={false}>
                     {(props, status) => 
@@ -37,8 +37,16 @@ it ('Debounce engine works fine', (done) => {
         </Form>
     )
 
-    form.find('input').simulate('change', { target: { value: 'value' }});
-    expect(form.find('input').props().value).toBe('value')
+    form.find('input').simulate('change', { target: { value: 'a' }});
+    expect(form.find('input').props().value).toBe('a')
+    expect(form.state()).not.toHaveProperty('fieldValues.first.second')
+
+    form.find('input').simulate('change', { target: { value: 'ab' }});
+    expect(form.find('input').props().value).toBe('ab')
+    expect(form.state()).not.toHaveProperty('fieldValues.first.second')
+
+    form.find('input').simulate('change', { target: { value: 'abc' }});
+    expect(form.find('input').props().value).toBe('abc')
     expect(form.state()).not.toHaveProperty('fieldValues.first.second')
 
     setTimeout(() => {
@@ -47,7 +55,7 @@ it ('Debounce engine works fine', (done) => {
     }, 300);
 })
 
-it ('Form group prefixes field names', () => {
+it ('Form group prefixes nested fields fieldnames', () => {
 
     const form = mount(
         <Form>
@@ -67,12 +75,62 @@ it ('Form group prefixes field names', () => {
     expect(form.state()).toHaveProperty('fieldValues.group.field')
 })
 
+/**
+ * Form Tests
+ * 
+ * General
+ *  it shows the inspector correcly
+ *  it sets the inical values correcly
+ *  it sinalize on any field change
+ *  it works in stateless mode
+ *
+ * Outside controll
+ *  it sets field value programactly
+ *  it removes a field programactly
+ *  it validates
+ *  it submits
+ *  
+ * Validations
+ *  it only submits if all fields doesnt contains errors
+ * 
+ * Form Status
+ *  it detects if form is dirty
+ *  it detects if any field is validating
+ *  it detects that all fields has validated
+ *  it detects if any fields has errors
+ */
 
-// Valor
-// Debounce
-// FieldStatus
-// FieldGroup
-// Errors
+/** 
+ * FieldGroup tests
+ * 
+ * it Prefixes child fields
+ * it Respects form boudary
+*/
 
+/** 
+ * Field tests
+ * 
+ * General
+ *  it shows the inspector correcly
+ *  it debounce engine works right
+ * 
+ * Validation
+ *  it executes validations
+ * 
+ * FieldStatus
+ *  it sinalizes when field was touched
+ *  it sinalizes when field is dirty
+ *  it sinalizes when field has erros
+ *  it sinalizes when field is validating
+ * 
+ * Passthrough props
+ *  it Passesthrough OnClick
+ *  it Passesthrough OnChange
+ *  it Passesthrough OnBlur
+ *  it Passesthrough OnFocus
+ * 
+ * Factory
+ *  it sums properties
+ */
 
 export default {}
