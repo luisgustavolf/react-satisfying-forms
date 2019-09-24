547b2ceb630f490f9f154e4e79676fc6a8d82157
Luis Gustavo <luis.furtado@mob2con.com.br>
2019-09-24 16:47:30 -0300

    stage

--- dev/src/react-satisfying-forms-staless/validations/formValidationManager.tsx
+++ dev/src/react-satisfying-forms-staless/validations/formValidationManager.tsx
@@ -1,37 +1,40 @@
 import { FieldValidationManager } from "./fieldValidatonManager";
-import { IFieldValidations } from '../interfaces/iFieldValidations';
-import { flattenObject } from '../util/objectUtil';
-import { IFieldValidation } from "../interfaces/iFieldValidation";
+import { IFieldValidator } from "../interfaces/iFieldValidator";
 import * as OPath from 'object-path';
+import { isPrimitive } from "util";
+
+export interface IValidationResult {
+    fieldName: string,
+    errors: string[]
+}
 
 export interface FieldValidationWithValidationMng<TData>  {
     fieldname: string
-    validations: IFieldValidation<TData> 
+    validators: IFieldValidator[]
     validationManager: FieldValidationManager
 }
 
 export class FormValidationManager<TData> {
 
     fieldsValidations: FieldValidationWithValidationMng<TData>[] = []
-    
-    registerFieldsValidations(fieldValidations: IFieldValidations<TData>) {
-        const flattendFieldValidations = flattenObject(fieldValidations)
-        for (const key in flattendFieldValidations) {
-            if (flattendFieldValidations.hasOwnProperty(key)) {
-                this.registerFieldValidations(key, flattendFieldValidations[key])
-            }
+
+    unRegisterFieldValidations(fieldname: string) { 
+        const pos = this.fieldsValidations.findIndex((i) => i.fieldname == fieldname);
+        
+        if (pos > -1) {
+            this.fieldsValidations.splice(pos, 1);
         }
     }
-
-    registerFieldValidations(fieldname: string, validations: IFieldValidation<TData>) {
+    
+    registerFieldValidations(fieldname: string, validators: IFieldValidator[]) {
         const fieldValidator = this.getFieldValidations(fieldname)
         
         if (fieldValidator)
-            fieldValidator.validations = validations
+            fieldValidator.validators = validators
         else
             this.fieldsValidations.push({
                 fieldname,
-                validations,
+                validators: validators,
                 validationManager: new FieldValidationManager()
             })
     }
@@ -42,5 +45,40 @@ export class FormValidationManager<TData> {
 
     getFieldsWithValidations() {
         return this.fieldsValidations.map((validations) => validations.fieldname)
+    }
+
+    async validateField(fieldName: string, value: any) {
+        const fieldValidator = this.getFieldValidations(fieldName)
+        
+        if (!fieldValidator) return;
+        
+        return await new Promise<IValidationResult>((resolve) => {
+            fieldValidator.validationManager.validate(value, fieldValidator.validators, () => {}, (errors) => {
+                resolve({
+                    fieldName,
+                    errors: errors || []
+                })
+            })    
+        })
+    }
+
+    async validateFields(values?: any) {
+        if (!values) [];
+        
+        const runningValidations = this.fieldsValidations.map((fv) => {
+            const fieldValue = OPath.get(values, fv.fieldname);
+            return new Promise<IValidationResult>((resolve) => {
+                fv.validationManager.validate(fieldValue, fv.validators, () => {}, (errors) => { 
+                    resolve({
+                        fieldName: fv.fieldname,
+                        errors: errors || []
+                    }) 
+                })
+            })
+        })
+
+        const results = await Promise.all(runningValidations);
+
+        return results;
     }
 }
